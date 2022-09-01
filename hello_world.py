2c5bec1444038a8786e52b6cf782744686558c06
smit-darji <98390067+smit-darji@users.noreply.github.com>
2022-09-01 16:16:31 +0530

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,7 +2,7 @@ from threading import Event # Needed for the  wait() method
 from time import sleep     
 
 print("\n wait for 10 sec start")
-sleep(1000)               # Conventional sleep() Method.
+sleep(100)               # Conventional sleep() Method.
 print("\nafter 10 sec")   
 Event().wait(3.0) # wait() Method, useable sans thread.
 print("\n all Completed")
