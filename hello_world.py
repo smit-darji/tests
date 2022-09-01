875755b3fbe91b9bc6223deb7fa40a9b3ed48e35
smit-darji <98390067+smit-darji@users.noreply.github.com>
2022-09-01 16:19:43 +0530

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -4,5 +4,4 @@ from time import sleep
 print("\n wait for 10 sec start")
 sleep(100)               # Conventional sleep() Method.
 print("\nafter 10 sec")   
-Event().wait(3.0) # wait() Method, useable sans thread.
-print("\n all Completed")
+
